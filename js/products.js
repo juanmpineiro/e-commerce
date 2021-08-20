47c07edbb2638915c7682d8c0c44671bfda2d6f8
juanmpineiro <88333738+juanmpineiro@users.noreply.github.com>
2021-08-20 13:43:55 -0300

    aaa

--- js/products.js
+++ js/products.js
@@ -1,51 +1,10 @@
-const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_BY_PROD_PRICE_ASC = "Precio";
 var listaAutos = []
-var currentSortCriteria = undefined;
-var currentProductArray = [];
-var minPrice = undefined;
-var maxPrice = undefined;
-
-// Función para ordenar los productos según el filtro elegido.
-function sortProducts(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if (a.name < b.name){ return -1; }
-            if (a.name > b.name){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if (a.name > b.name){ return -1; }
-            if (a.name < b.name){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_PROD_PRICE_ASC){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.cost);
-            let bCount = parseInt(b.cost);
-
-            if (aCount > bCount){ return -1; }
-            if (aCount < bCount){ return 1; }
-            return 0;
-        });
-    }
-
-    return result;
-}
 
 // Función que muestra cada producto de la lista.
 function showProductList(){
     let htmlContentToAppend = "";
     for (let i = 0; i < listaAutos.length; i++) {
-        let product = listaAutos[i];
 
-        // Filtra los productos por precio.
-        if (((minPrice == undefined) || (minPrice != undefined && parseInt(listaAutos[i].cost) >= minPrice)) &&
-            ((maxPrice == undefined) || (maxPrice != undefined && parseInt(listaAutos[i].cost) <= maxPrice))){
 
         // Codigo HTML para cada producto de la lista.     
         htmlContentToAppend += `
@@ -71,22 +30,6 @@ function showProductList(){
     }
         document.getElementById("listaproductos").innerHTML = htmlContentToAppend;
     }
-}
-
-// Función que filtra y luego muestra la lista.
-function sortAndShowProducts(sortCriteria, productArray){
-    currentSortCriteria = sortCriteria;
-
-    if(productArray != undefined){
-        currentProductArray = productArray;
-    }
-
-    currentProductArray = sortProducts(currentSortCriteria, currentProductArray);
-
-
-    showProductList();
-}
-
 
 
 // Cuando la página termina de cargar se ejecuta y muestra lo primero que vemos cuando entramos
@@ -95,60 +38,10 @@ document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function (resultObj) {
         if (resultObj.status === "ok") {
             listaAutos = resultObj.data;
-            
-            sortAndShowProducts(ORDER_ASC_BY_NAME, resultObj.data);
+            showProductList()
+
+        } else{
+            console.log("hola")
         }
-    });
-    
-    // Detecta cuando hacemos click en el botón y ejecuta la funcion 'sortAndShowProducts' con el filtro que hayamos elegido.
-    document.getElementById("sortAsc").addEventListener("click", function(){
-        sortAndShowProducts(ORDER_ASC_BY_NAME);
-    });
-
-    document.getElementById("sortDesc").addEventListener("click", function(){
-        sortAndShowProducts(ORDER_DESC_BY_NAME);
-    });
-
-    document.getElementById("sortByPrice").addEventListener("click", function(){
-        sortAndShowProducts(ORDER_BY_PROD_PRICE_ASC);
-    });
-
-    document.getElementById("rangeFilterPrice").addEventListener("click", function(){
-        
-
-        minPrice = document.getElementById("rangeFilterPriceMin").value;
-        maxPrice = document.getElementById("rangeFilterPriceMax").value;
-
-        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
-            minPrice = parseInt(minCount);
-        }
-        else{
-            minPrice = undefined;
-        }
-
-        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
-            maxPrice = parseInt(maxCount);
-        }
-        else{
-            maxPrice = undefined;
-        }
-
-        showProductList();
-    });
-
-    // Borra lo que escribimos en el apartado 'Filtrar por precio' y nos muestra la pagina con la lista de productos original.
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
-        document.getElementById("rangeFilterPriceMin").value = "";
-        document.getElementById("rangeFilterPriceMax").value = "";
-
-        minCount = undefined;
-        maxCount = undefined;
-
-        showProductList();
-    });
-
-    
-});
-
-
-
+    }   
+    )})
